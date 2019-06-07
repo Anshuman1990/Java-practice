b1c8d12925e128e1c13db292980af0fd70e8938a
Anshuman Saha <anshuman1990@users.noreply.github.com>
2019-06-07 23:39:13 +0530

    Java Streams Practice

---
+++ src/com/language/streams/Main.java
@@ -0,0 +1,13 @@
+package com.language.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        functionalTest test = () -> "";
+
+        List list = new ArrayList();
+
+    }
+}
---
+++ src/com/language/streams/functionalTest.java
@@ -0,0 +1,7 @@
+package com.language.streams;
+
+@FunctionalInterface
+public interface functionalTest {
+
+    public abstract String test();
+}
